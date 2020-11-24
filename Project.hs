b95fb00ba363d7f977e59df6c55e1a22b051757e
KobeVdL <45506839+KobeVdL@users.noreply.github.com>
2020-11-24 08:45:28 +0100

    Add files via upload

--- Project.hs
+++ Project.hs
@@ -1,5 +1,10 @@
 import Data.Set (Set)
+import Data.Map (Map)
+import Data.Maybe (Maybe)
+
 import qualified Data.Set as Set
+import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
 
 data Kardinaliteit = Integer deriving (Eq, Show)
 
@@ -17,7 +22,16 @@ data Term = Empty |
             kardinaliteit :: Integer}  |
             Variable {
             name :: String}
-            deriving (Eq, Show) {- Een term bevat een 
+            deriving (Eq, Show)
+
+
+--Elke buitenste is een predikaat deze bevat termen als argumenten
+data Predicate = MkPredicate {
+    nameOfPred :: String ,
+    kardinaliteitOfPred :: Integer ,
+    valuesOfPred :: [Term] }
+    deriving (Eq, Show)
+            {- Een term bevat een 
 naam de kardinaliteit (of het aantal termen het bezit) en de bijhorende termen 
 Empty geeft een fout aan, of een valse staat.
 -}
@@ -25,85 +39,71 @@ instance Ord Term where -- verwacht namen zijn uniek
     term1 `compare` term2  = (name term1) `compare` (name term2)
 
 
-data Binder = MkBinder {
-            getBinder :: [(Term,Term)]
-            } | InvalidBinder deriving (Eq, Show)
+type Binder = Map Term Term
 
 
 --data Value a = MkValue a deriving (Eq, Show)
 
 data Clause = Rule
-              {headTerm :: Term ,
-              body :: [Term] }
+              {headTerm :: Predicate ,
+              body :: [Predicate] }
               | Fact 
-              {headTerm :: Term}    
-              | InvalidClause
+              {headTerm :: Predicate}    
               deriving (Eq, Show)
 
-data Scheme = MkScheme { 
-                  relation :: String ,
-                  getSchemeValues :: (Set [Term])
-                  } 
-               | InvalidScheme deriving (Eq, Show)-- zie datalog hou bij welke values een bepaald predikaat heeft.
+type Scheme = Map String (Set [Term])
 
 data Program = MkProgram [Clause]
-
-fireAllRules :: Program -> [Scheme] -> [Term]
-
-fireAllRules (MkProgram []) listOfSchemes = []
-
-fireAllRules (MkProgram (firstClause:restOfClauses)) listOfSchemes = 
-    (findNewTerms firstClause listOfSchemes) ++ (fireAllRules (MkProgram restOfClauses) listOfSchemes)
-
     
-test3 = print (addToScheme [t4] m )
-        where
-        t1 = Constant "a" 0 
-        t2 = Constant "b" 0
-        t3 = Constant "c" 0
-        t4 = MkTerm "l" 1 [t3]         
-        m = MkScheme "a" (Set.fromList [[t1],[t2]])
 
-test4 = print (addToCorrectScheme t3 [m1,m2,m3])
+test4 = print (addToCorrectScheme p3 m3)
         where
         t1 = Constant "a" 0 
         t2 = Constant "c" 0
         t3 = MkTerm "b" 1 [t1] 
-        m1 = MkScheme "a" (Set.fromList [])
-        m2 = MkScheme "b" (Set.fromList [])
-        m3 = MkScheme "c" (Set.fromList [])
+        p1 = MkPredicate "a" 1 [t1]
+        p2 = MkPredicate "b" 1 [t2]
+        p3 = MkPredicate "c" 1 [t3]
+        m =  Map.empty
+        m1 = Map.insert "a" (Set.empty) m 
+        m2 = Map.insert "b" (Set.empty) m1
+        m3 = Map.insert "c" (Set.empty) m2
+
         
-test5 = print (show (unification t1 t2 r))
+{-test5 = print (show (unification t1 t2 r))
     where
     t1 = Variable "a"  
     t2 = Constant "b" 0 
-    t3 = MkTerm "b" 1 [t1] 
+    --t1 = MkPredicate "elder" 1 [b]
+    --t2 = MkPredicate "elder" 1 [a]
+    --t3 = MkPredicate "c" 1 [b] 
     r = Rule t2 [t3,t2] 
-    
-test6 = print (show (findNewTerms r [scheme]))
+  -}  
+test6 = print (show (findNewPred r m1))
     where 
     t1 = Constant "a" 0 
-    t2 = MkTerm "b" 1 [Variable "x"]
-    t3 = MkTerm "c" 1 [Variable "x"] 
+    t2 = MkPredicate "b" 1 [Variable "x"]
+    t3 = MkPredicate "c" 1 [Variable "x"] 
     r = Rule t3 [t2]
-    scheme = MkScheme "b" (Set.fromList [[t1]])
+    m = Map.empty
+    m1 = Map.insert "b" (Set.fromList [[t1]]) m 
     
-test7 = print (bottom_upAlgorithm program 1)
+test7 = print (bottom_upAlgorithm program 10)
     where
-    p1 = MkTerm "Person" 1 [(Constant "ann" 0)]
-    p2 = MkTerm "Person" 1 [(Constant "betrand" 0)]
-    p3 = MkTerm "Person" 1 [(Constant "charles" 0)]
-    p4 = MkTerm "Person" 1 [(Constant "dorothy" 0)]
-    p5 = MkTerm "Person" 1 [(Constant "evelyn" 0)]
-    p6 = MkTerm "Person" 1 [(Constant "fred" 0)]
-    p7 = MkTerm "Person" 1 [(Constant "george" 0)]
-    p8 = MkTerm "Person" 1 [(Constant "hilary" 0)]
-    pa1 = MkTerm "Parent" 2 [(Constant "dorothy" 0),(Constant "george" 0)]
-    pa2 = MkTerm "Parent" 2 [(Constant "evelyn" 0),(Constant "george" 0)]
-    pa3 = MkTerm "Parent" 2 [(Constant "betrand" 0),(Constant "dorothy" 0)]
-    pa4 = MkTerm "Parent" 2 [(Constant "ann" 0),(Constant "dorothy" 0)]
-    pa5 = MkTerm "Parent" 2 [(Constant "ann" 0),(Constant "hilary" 0)]
-    pa6 = MkTerm "Parent" 2 [(Constant "charles" 0),(Constant "evelyn" 0)]
+    p1 = MkPredicate "Person" 1 [(Constant "ann" 0)]
+    p2 = MkPredicate "Person" 1 [(Constant "betrand" 0)]
+    p3 = MkPredicate "Person" 1 [(Constant "charles" 0)]
+    p4 = MkPredicate "Person" 1 [(Constant "dorothy" 0)]
+    p5 = MkPredicate "Person" 1 [(Constant "evelyn" 0)]
+    p6 = MkPredicate "Person" 1 [(Constant "fred" 0)]
+    p7 = MkPredicate "Person" 1 [(Constant "george" 0)]
+    p8 = MkPredicate "Person" 1 [(Constant "hilary" 0)]
+    pa1 = MkPredicate "Parent" 2 [(Constant "dorothy" 0),(Constant "george" 0)]
+    pa2 = MkPredicate "Parent" 2 [(Constant "evelyn" 0),(Constant "george" 0)]
+    pa3 = MkPredicate "Parent" 2 [(Constant "betrand" 0),(Constant "dorothy" 0)]
+    pa4 = MkPredicate "Parent" 2 [(Constant "ann" 0),(Constant "dorothy" 0)]
+    pa5 = MkPredicate "Parent" 2 [(Constant "ann" 0),(Constant "hilary" 0)]
+    pa6 = MkPredicate "Parent" 2 [(Constant "charles" 0),(Constant "evelyn" 0)]
     f1 = Fact p1
     f2 = Fact p2
     f3 = Fact p3
@@ -118,10 +118,10 @@ test7 = print (bottom_upAlgorithm program 1)
     f11 = Fact pa4
     f12 = Fact pa5
     f13 = Fact pa6
-    r1 = Rule (MkTerm "sgc" 2 [(Variable "X"),(Variable "X")]) [(MkTerm "Person" 1 [(Variable "X")] )]
-    r2 = Rule (MkTerm "sgc" 2 [(Variable "X"),(Variable "Y")]) [(MkTerm "Parent" 2 [(Variable "X"),(Variable "X1")] ),
-        (MkTerm "sgc" 2 [(Variable "X1"),(Variable "Y1")]),
-        (MkTerm "Parent" 2 [(Variable "Y"),(Variable "Y1")] )]
+    r1 = Rule (MkPredicate "sgc" 2 [(Variable "X"),(Variable "X")]) [(MkPredicate "Person" 1 [(Variable "X")] )]
+    r2 = Rule (MkPredicate "sgc" 2 [(Variable "X"),(Variable "Y")]) [(MkPredicate "Parent" 2 [(Variable "X"),(Variable "X1")] ),
+        (MkPredicate "sgc" 2 [(Variable "X1"),(Variable "Y1")]),
+        (MkPredicate "Parent" 2 [(Variable "Y"),(Variable "Y1")] )]
     program = MkProgram [f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13,f14,r1,r2]
     
 {-Manier om juiste variabele te zoeken: 1 check of alle elementen in regel ook in bottom-up set zitten
@@ -134,136 +134,131 @@ test7 = print (bottom_upAlgorithm program 1)
 
 --Unificatie denk let Martelli - montenari algoritme
 
-findNewTerms :: Clause -> [Scheme] -> [Term]
 
-findNewTerms (Fact x) _ = [x]
 
-findNewTerms InvalidClause schemeList =[]
+getSchemeValues ::  String -> Scheme -> Maybe (Set [Term])
 
-findNewTerms (Rule x []) schemeList = [x] 
+getSchemeValues relation scheme = Map.lookup relation scheme 
 
-findNewTerms (Rule x (y:ys)) schemeList 
-    | (z /= InvalidScheme) =(foldl (\acc a -> let
-        binder = findBinderArguments (values y) (a)
-        newRule = bindRule binder (Rule x ys)
-        in
-        (findNewTerms newRule schemeList )++ acc) []) (getSchemeValues z)
-    | otherwise = []
-    where 
-    z = findCorrectScheme y schemeList  
-    
-findCorrectScheme :: Term -> [Scheme] -> Scheme
 
-findCorrectScheme term [] = InvalidScheme
+findNewPred :: Clause -> Scheme -> [Predicate]
 
-findCorrectScheme term (x:xs) 
-    | (name term) == (relation x) = x
-    | otherwise = findCorrectScheme term xs
+findNewPred (Fact x) _ = [x]
 
-unification :: Term -> Term -> Clause ->  Clause --(moet variabele allemaal vervangen zodat 2 gekozen termen gelijk zijn)
+findNewPred (Rule x []) scheme = [x] 
 
-unification term1 term2 rule
-    --  | (unificationPossible term1 term2) =  rule --TODO gebruik binder om alle variabele te veranderen + check of bij find binder dat hij nog niet in de lijst zit
-    -- | otherwise = rule
-    | z /= InvalidBinder  = bindRule z rule
-    | otherwise = InvalidClause -- geef onmogelijk resultaat terug als er niet kan gebindt worden
-    where 
-    z= (findBinders term1 term2)
-    
-bindRule :: Binder -> Clause -> Clause
 
-bindRule InvalidBinder _ =InvalidClause
-
-bindRule (MkBinder []) rule = rule --binder overlopen
-
-bindRule (MkBinder ((x,y): xs)) rule = bindRule (MkBinder xs) z
+findNewPred (Rule x (y:ys)) scheme =case z of
+    Just posValueOfy -> (foldl (\acc a -> let
+        binder = findBinderArguments (valuesOfPred y) a
+        newRule = bindRule binder (Rule x ys)    in
+        (case newRule of 
+        Just rule -> (findNewPred rule scheme )++ acc
+        Nothing -> acc)) [] posValueOfy)
+    Nothing -> []
     where
-    z = changeVariableOfRule rule x y 
+    z = getSchemeValues (nameOfPred y) scheme
     
-findBinders :: Term -> Term -> Binder 
+  
 
-findBinders (Variable x) bindingTerm = MkBinder [((Variable x) ,bindingTerm)]
+{-
+unification :: Term -> Term -> Clause ->  Maybe Clause --(moet variabele allemaal vervangen zodat 2 gekozen termen gelijk zijn)
+
+unification term1 term2 rule = bindRule z rule
+    where 
+    z= findBinders term1 term2
+    
+ -}   
+    
+bindRule :: Maybe Binder -> Clause -> Maybe Clause
+
+bindRule Nothing rule = Nothing
+
+bindRule (Just binder) rule = Just (Map.foldrWithKey (changeVariableOfRule) rule binder) 
+
+
+    
+    
+    
+findBinders :: Term -> Term -> Maybe Binder 
+
+findBinders (Variable x) bindingTerm = Just (Map.singleton (Variable x) bindingTerm )
 
 findBinders (Constant x 0) bindingTerm 
-    | (name bindingTerm) == x = MkBinder []
-    | otherwise = InvalidBinder
+    | (name bindingTerm) == x = Just Map.empty
+    | otherwise = Nothing 
 
 findBinders term bindingTerm 
     | (name term) == (name bindingTerm) = findBinderArguments (values term) (values bindingTerm)
-    | otherwise = InvalidBinder
+    | otherwise = Nothing
     
-findBinderArguments :: [Term] -> [Term] -> Binder --Pre unificatie is mogelijk
+    
+    
+findBinderArguments :: [Term] -> [Term] -> Maybe Binder --Pre unificatie is mogelijk
 
-findBinderArguments [] [] = MkBinder []
+findBinderArguments [] [] = Just Map.empty
 
-findBinderArguments (x:xs) (y:ys) 
-    | (z /= InvalidBinder) && (k/= InvalidBinder) = appendBinder z k --Binder ((getVar z): (getVar k)) ((getActualValue z): (getActualValue k))
-    | otherwise = InvalidBinder
+findBinderArguments (x:xs) (y:ys) = appendBinder z k
     where
     z = findBinders x y 
     k = findBinderArguments xs ys
     
-appendBinder :: Binder -> Binder -> Binder
+    
+appendBinder :: Maybe Binder -> Maybe Binder -> Maybe Binder
 
-appendBinder (MkBinder []) binder =binder
-
-appendBinder (MkBinder ((var,val):xs)) binder 
-    | (z == InvalidBinder) = InvalidBinder
-    | otherwise = appendBinder (MkBinder xs) z
-    where
-    z = addToBinder (var,val) binder
+appendBinder binder1 binder2 = do 
+    x <- binder1
+    y <- binder2
+    Just (Map.union x y)
 
 
 addToBinder :: (Term,Term) -> Binder -> Binder
 
-addToBinder (var1,val1) (MkBinder []) = MkBinder [(var1,val1)]
-
-addToBinder (var1,val1) (MkBinder ((var2,val2):xs))
-    | (var1 == var2) && (val1 == val2) = MkBinder ((var2,val2):xs)
-    | (var1 == var2) = InvalidBinder
-    | (z == InvalidBinder) = InvalidBinder
-    | otherwise =MkBinder ((var1,val1):(var2,val2):xs)
-    where
-    z = addToBinder  (var1,val1) (MkBinder (xs))
-
-
-addToScheme :: [Term] -> Scheme -> Scheme
-
-addToScheme termList s = x
-    where 
-    x = MkScheme (relation s) (Set.insert termList (getSchemeValues s) )
+addToBinder ((Variable v),term) binder = Map.insert (Variable v) term binder
+ 
+    
 
 -- zoek de scheme waar waarde moet toegevoegd worden
-addToCorrectScheme :: Term -> [Scheme] -> [Scheme] 
+addToCorrectScheme :: Predicate -> Scheme -> Scheme 
 
-addToCorrectScheme (Constant _ _) (x:xs) = error "Constante waarde" -- Zou error moeten geven
+--addToCorrectScheme (Constant _ _) scheme = error "Constante waarde" -- Zou error moeten geven
 
-addToCorrectScheme (Variable _) (x:xs) = error "Variabele waarde"
+--addToCorrectScheme (Variable _) scheme = error "Variabele waarde"
 
-addToCorrectScheme term [] = (MkScheme (name term) (Set.fromList [(values term)])):[]
+addToCorrectScheme pred m = Map.insertWith (Set.union) (nameOfPred pred) (Set.insert (valuesOfPred pred) Set.empty) m
 
-
-
-addToCorrectScheme term (x:xs)
-    |(name term) == (relation x) = (addToScheme (values term) x) : xs
-    | otherwise = x : addToCorrectScheme term xs
  
-addListToCorrectScheme :: [Term] -> [Scheme] -> [Scheme]
+addListToCorrectScheme :: [Predicate] -> Scheme -> Scheme
 
-addListToCorrectScheme [] schemeList = schemeList
+addListToCorrectScheme [] scheme = scheme
 
-addListToCorrectScheme (x:xs) schemeList = addListToCorrectScheme xs z
+addListToCorrectScheme (x:xs) scheme = addListToCorrectScheme xs z
     where 
-    z =addToCorrectScheme x schemeList
+    z =addToCorrectScheme x scheme
 
 
-changeVariableOfRule :: Clause -> Term -> Term -> Clause
 
-changeVariableOfRule r (Variable x) bindingTerm = Rule (changeVariable (headTerm r) (Variable x) bindingTerm) (changeVariableTermList (body r) (Variable x) bindingTerm)
+changeVariableOfRule ::   Term -> Term -> Clause  -> Clause
 
-changeVariableOfRule r _ bindingTerm = error "Verwacht een Variabele geen term"
+changeVariableOfRule (Variable x) bindingTerm  r  = Rule (changeVariableInPredicate (headTerm r) (Variable x) bindingTerm) (changeVariableInPredicateList (body r) (Variable x) bindingTerm)
 
-changeVariable :: Term ->  Term -> Term ->Term
+changeVariableOfRule _ bindingTerm r = error "Verwacht een Variabele geen term"
+
+
+
+changeVariableInPredicate :: Predicate -> Term -> Term -> Predicate
+
+changeVariableInPredicate (MkPredicate name kard termList) var replacingTerm = MkPredicate name kard (changeVariableTermList termList var replacingTerm)
+
+
+changeVariableInPredicateList :: [Predicate] -> Term -> Term -> [Predicate]
+
+changeVariableInPredicateList [] var bindingTerm = []
+
+changeVariableInPredicateList (y:ys) var bindingTerm = (changeVariableInPredicate y var bindingTerm) : (changeVariableInPredicateList ys var bindingTerm)
+
+
+changeVariable :: Term ->  Term -> Term -> Term
 
 changeVariable (Variable y) (Variable x) bindingTerm = 
                 if ((Variable x)==(Variable y)) then
@@ -276,6 +271,8 @@ changeVariable (MkTerm nameTerm kard valueOfTerm) (Variable x) bindingTerm = MkT
     
 changeVariable termToChange _ bindingTerm = error "Verwacht een Variabele geen term"
 
+
+
 changeVariableTermList :: [Term] -> Term -> Term -> [Term]
 
 changeVariableTermList (y:ys) (Variable x) bindingTerm = (changeVariable y (Variable x) bindingTerm) : (changeVariableTermList ys (Variable x) bindingTerm)
@@ -284,17 +281,29 @@ changeVariableTermList [] (Variable x) bindingTerm = []
 
 changeVariableTermList _ _ _ = error "Verwacht een Variabele geen term"
 
-bottom_upAlgorithm :: Program -> Integer ->  [Scheme]     
 
-bottom_upAlgorithm _ 0  = []
 
-bottom_upAlgorithm program integer = bottom_upHelper program integer [] 
+fireAllRules :: Program -> Scheme -> [Predicate]
 
-bottom_upHelper:: Program -> Integer ->  [Scheme] -> [Scheme]
+fireAllRules (MkProgram []) listOfSchemes = []
 
-bottom_upHelper _ 0 schemeList = schemeList
+fireAllRules (MkProgram (firstClause:restOfClauses)) scheme = 
+    (findNewPred firstClause scheme) ++ (fireAllRules (MkProgram restOfClauses) scheme)
 
-bottom_upHelper program n schemeList = bottom_upHelper program (n-1) newSchemeList
+
+bottom_upAlgorithm :: Program -> Integer ->  Scheme 
+
+bottom_upAlgorithm _ 0  = Map.empty 
+
+bottom_upAlgorithm program integer = bottomUpHelper program integer Map.empty 
+
+
+
+bottomUpHelper:: Program -> Integer ->  Scheme -> Scheme
+
+bottomUpHelper _ 0 scheme = scheme
+
+bottomUpHelper program n scheme = bottomUpHelper program (n-1) newScheme
     where
-    newTerms = fireAllRules program schemeList
-    newSchemeList = addListToCorrectScheme newTerms schemeList
+    newPreds = fireAllRules program scheme
+    newScheme = addListToCorrectScheme newPreds scheme
