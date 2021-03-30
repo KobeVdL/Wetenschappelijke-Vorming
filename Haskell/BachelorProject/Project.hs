13a6938a24499d31a5139110c0322eb66d2d5da0
KobeVdL <45506839+KobeVdL@users.noreply.github.com>
2021-03-30 15:23:16 +0200

    Add files via upload

--- Haskell/BachelorProject/Project.hs
+++ Haskell/BachelorProject/Project.hs
@@ -5,9 +5,11 @@ module Project where
 import BasicProlog
 import Shuffle
 import TopDown
+import TopDownBackTrack
 import Data.Set (Set)
 import Data.Map (Map)
 import Data.Maybe (Maybe)
+import Data.List
 import Debug.Trace
 import System.IO.Unsafe
 import PropertyChecking
@@ -16,10 +18,10 @@ import qualified Data.Set as Set
 import qualified Data.Map as Map
 import qualified Data.Maybe as Maybe
 
+-- FOUT ontdekt leq staat er niet tussen
+semiNaiveBottomUp :: Program -> Int -> Scheme -> Scheme -> Scheme
 
-semiNaiveBottomUp :: Program -> Integer -> Scheme -> Scheme -> Scheme
-
-semiNaiveBottomUp _ 0 oldScheme differenceScheme = unionScheme oldScheme differenceScheme
+semiNaiveBottomUp _ (-1) oldScheme differenceScheme = unionScheme oldScheme differenceScheme
 
 semiNaiveBottomUp program n oldScheme differenceScheme = semiNaiveBottomUp program (n-1) newScheme newFoundElements
     where
@@ -29,60 +31,95 @@ semiNaiveBottomUp program n oldScheme differenceScheme = semiNaiveBottomUp progr
     allElements =  unionScheme newScheme foundElements
     newFoundElements =  Map.differenceWith (maybeSetDif) allElements newScheme 
     
--- Gaat steeds property checken na toepassen regel
-fastBottomUpProperty :: Program -> Integer -> Scheme -> Scheme -> Bool    
+-- Gaat steeds property checken na toepassen regel, Return True als het algoritme stopt met dat de property false is, (anders geef foutief resultaat)
+fastBottomUpProperty :: Program ->  (Predicate -> Bool) -> Int -> Scheme -> Scheme -> IO (Maybe Predicate)  
     
-fastBottomUpProperty _ 0 oldScheme differenceScheme = True   
+fastBottomUpProperty _ propCheck (-1) oldScheme differenceScheme = return Nothing  
     
-fastBottomUpProperty program n oldScheme differenceScheme = 
+fastBottomUpProperty program propCheck n oldScheme differenceScheme = 
     case prop of
-        True -> fastBottomUpProperty program (n-1) newScheme newFoundElements
+        Nothing -> do
+            fastBottomUpProperty program propCheck (n-1) newScheme newFoundElements
             where
             foundElements =  addListToCorrectScheme newPreds Map.empty
             newScheme =    unionScheme oldScheme differenceScheme
             allElements =  unionScheme newScheme foundElements
             newFoundElements =  Map.differenceWith (maybeSetDif) allElements newScheme  
-        False -> False
+        Just pred -> return (Just pred)
     where
-    (prop,newPreds) = fireAllRulesProperty program oldScheme differenceScheme
+    (prop,newPreds) = fireAllRulesProperty program propCheck oldScheme differenceScheme
 
 
 
 
-fireAllRulesProperty :: Program -> Scheme -> Scheme -> (Bool,[Predicate])
+fireAllRulesProperty :: Program -> (Predicate -> Bool) -> Scheme -> Scheme -> (Maybe Predicate,[Predicate])
 
-fireAllRulesProperty (MkProgram []) oldScheme differenceScheme = (True,[])
+fireAllRulesProperty (MkProgram []) propCheck oldScheme differenceScheme = (Nothing,[])
 
-fireAllRulesProperty (MkProgram (firstClause:restOfClauses)) oldScheme differenceScheme = 
+fireAllRulesProperty (MkProgram (firstClause:restOfClauses)) propCheck oldScheme differenceScheme = 
     case prop of
-        False -> (False ,[]) -- Als property false is mogen we stoppen
-        True -> case propNext of
-            True -> (True, clausePred ++ list)
-            False -> (False ,[])
+        Just pred -> (Just pred,[]) -- Als tegenvoorbeeld gevonden is mogen we stoppen
+        Nothing -> case propNext of
+            Just predNext -> (Just predNext,[])--(True, clausePred ++ list)
+            Nothing -> (Nothing,clausePred ++ list)--(False ,[]) -- add the Next found predicates to a list to use them later
             where 
-            (propNext,list) =fireAllRulesProperty (MkProgram restOfClauses) oldScheme differenceScheme
+            (propNext,list) =fireAllRulesProperty (MkProgram restOfClauses) propCheck oldScheme differenceScheme
     where
     clausePred = findNewPred firstClause oldScheme differenceScheme
-    prop = and (map (\x -> checkProperty x step) clausePred) 
+    prop =find (propCheck) clausePred--and (map (\x -> checkProperty x func) clausePred) 
 
 
  
     
     
-checkPropertyScheme :: Scheme -> (String-> Term-> Maybe Term) -> Bool
+checkPropertyScheme :: Scheme -> (Predicate -> Bool) -> Bool
     
-checkPropertyScheme scheme func = listPred
+checkPropertyScheme scheme propCheck = listPred
     where
     listKeys = Map.toList scheme
-    listPred = foldr (\(x,y) acc -> (Set.foldr (\z acc2-> (checkProperty (MkPredicate x (length z) z) func) 
+    listPred = foldr (\(x,y) acc -> (Set.foldr (\z acc2-> (propCheck (MkPredicate x (length z) z)) 
         && acc2 ) True y ) && acc) True  listKeys
     -- predList = Map.mapWithKey (\name x -> (Set.map MkPredicate name (length x) x )) scheme
 
 
+
+--Return True als het algoritme stopt met dat de property false is, (anders geef foutief resultaat)
+topDownPropertyChecking :: Int-> Int -> (Predicate -> Bool) -> Program -> IO (Maybe Predicate)
+
+topDownPropertyChecking _ 0 propCheck program = return Nothing
+
+topDownPropertyChecking size n propCheck program = do
+    maybePred <- topDownAlgorithm ([MkPredicate "hasType" 2 [Variable "X1",Variable "Z2"]]) size (serperateConstant program) depthFirst 0
+    case maybePred of
+        Just pred-> do
+            let test = propCheck (head pred)
+            if test == False then 
+                return (Just (head pred))
+            else do
+                topDownPropertyChecking size (n-1) propCheck program
+        Nothing -> do 
+            putStrLn "Geen regels toepasbaar"
+            return Nothing
+
+--Return True als het algoritme stopt met dat de property false is, (anders geef foutief resultaat)
+topDownBacktrackPropertyChecking :: Int-> Int -> Int->(Predicate -> Bool) -> Program -> IO (Maybe Predicate)
+
+topDownBacktrackPropertyChecking _ 0 bStep propCheck program = return Nothing
+
+topDownBacktrackPropertyChecking size n bStep propCheck program = do
+    maybePred <- topDownBacktrackAlgorithm([MkPredicate "hasType" 2 [Variable "X1",Variable "Z2"]]) size (serperateConstant program) depthFirst 0 0
+    let prop = find (\x-> propCheck (head x)) maybePred --foldr (\x acc -> ((checkProperty (head x) func) && acc)) True maybePred 
+    case prop of
+        Just prop -> return (Just (head prop))
+        Nothing -> topDownBacktrackPropertyChecking size (n-1) bStep propCheck program
+
+
+
+
     
-checkProperty :: Predicate  -> (String-> Term-> Maybe Term) -> Bool    
+checkProperty :: (String-> Term-> Maybe Term) -> Predicate -> Bool    
    
-checkProperty pred  func = property1 term func && property2 term func && property3 pred func
+checkProperty func pred= property1 term func && property2 term func && property3 pred func
     where
     term = head (valuesOfPred pred)
    
--- Haskell/BachelorProject/PropertyChecking.hs
+++ Haskell/BachelorProject/PropertyChecking.hs
@@ -22,6 +22,10 @@ step "False" _ = Nothing
 
 step "Zero" _ = Nothing
 
+step "A" _ = Nothing
+
+step "[]" _ = Nothing
+
 step "ifThenElse" term  
     | condition == "True" = Just t2
     | condition == "False" = Just t3
@@ -39,11 +43,253 @@ step "succ" term = do
     where
     t = head (values term)
     
-step "leq" term 
-    | not (normalForm t1) = do
+step "add" (MkTerm "add" 2 [t1,t2]) 
+    | not (normalForm t1 step) = do
+        t1'<-(step (name t1) t1)
+        return (MkTerm "add" 2 [t1',t2])
+    | not (normalForm t2 step) = do
+        t2'<-(step (name t2) t2)
+        return (MkTerm "add" 2 [t1,t2'])
+    | not (nv (name t1) t1) && not (nv (name t1) t1) = Nothing
+    | otherwise = Just (addTerms t1 t2)
+
+
+step "subtract" (MkTerm "subtract" 2 [t1,t2])  
+    | not (normalForm t1 step) = do
+        t1'<-(step (name t1) t1)
+        return (MkTerm "subtract" 2 [t1',t2])
+    | not (normalForm t2 step) = do
+        t2'<-(step (name t2) t2)
+        return (MkTerm "subtract" 2 [t1,t2'])
+    | not (nv (name t1) t1) && not (nv (name t1) t1) = Nothing
+    | otherwise = Just (subtractTerms t1 t2)
+    
+step "multiply" (MkTerm "multiply" 2 [t1,t2]) 
+    | not (normalForm t1 step) = do
+        t1'<-(step (name t1) t1)
+        return (MkTerm "multiply" 2 [t1',t2])
+    | not (normalForm t2 step) = do
+        t2'<-(step (name t2) t2)
+        return (MkTerm "multiply" 2 [t1,t2'])
+    | not (nv (name t1) t1) && not (nv (name t1) t1) = Nothing
+    | otherwise = Just (multiplyTerms t1 t2)    
+    
+    
+step "leq" (MkTerm "leq" 2 [t1,t2])
+    | not (normalForm t1 step) = do
+        t1'<-(step (name t1) t1)
+        return (MkTerm "leq" 2 [t1',t2])
+    | not (normalForm t2 step) =do
+        t2'<-(step (name t2) t2)
+        return (MkTerm "leq" 2 [t1,t2'])
+    | not (nv (name t1) t1) && not (nv (name t1) t1) = Nothing
+    | (name t1 == "Zero") = Just (MkTerm "True" 0 [])
+    | (name t2 == "Zero") = Just (MkTerm "False" 0 [])
+    | (name t1 == "succ") && (name t2 == "succ") = step "leq" (MkTerm "leq" 2 [head (values t1),head (values t2)])
+    | otherwise = Nothing
+
+step ":" (MkTerm ":" 2 [t1,t2]) 
+    | not (normalForm t1 step) = do
+        t1'<-(step (name t1) t1)
+        return (MkTerm ":" 2 [t1',t2])
+    | not (normalForm t2 step) =do
+        t2'<-(step (name t2) t2)
+        return (MkTerm ":" 2 [t1,t2'])
+    | otherwise = Nothing
+
+step "++" (MkTerm "++" 2 [t1,t2]) 
+    | not (normalForm t1 step) = do
+        t1'<-(step (name t1) t1)
+        return (MkTerm "++" 2 [t1',t2])
+    | not (normalForm t2 step) =do
+        t2'<-(step (name t2) t2)
+        return (MkTerm "++" 2 [t1,t2'])
+    | otherwise = Just (appendArrays t1 t2)
+
+    
+step _ _ = Nothing
+    
+
+--PRECONDITION both terms are simplified
+-- adds two terms to each other on a declarative way
+addTerms :: Term -> Term ->  Term
+
+addTerms (MkTerm "Zero" 0 []) otherTerm = otherTerm
+
+addTerms (MkTerm "succ" 1 [rest]) otherTerm = addTerms rest (succ [otherTerm]) 
+    where
+    succ = MkTerm "succ" 1 
+    
+
+
+--PRECONDITION both terms are simplified
+-- subtract two terms, returns zero if the second term is larger then the first
+subtractTerms :: Term -> Term -> Term
+
+subtractTerms (MkTerm "Zero" 0 []) otherTerm =  (MkTerm "Zero" 0 [])
+
+subtractTerms firstTerm (MkTerm "Zero" 0 []) =  firstTerm
+
+subtractTerms (MkTerm "succ" 1 [rest]) (MkTerm "succ" 1 [rest2]) = subtractTerms rest rest2 
+
+    
+    
+--PRECONDITION both terms are simplified
+-- multiply 2 terms by adding them n times the second term n is the number of the first term
+multiplyTerms :: Term -> Term -> Term
+
+multiplyTerms t1 t2 = multiplyTermsHelper t1 t2 (MkTerm "Zero" 0 [])
+
+multiplyTermsHelper :: Term -> Term ->Term -> Term
+
+
+multiplyTermsHelper (MkTerm "Zero" 0 []) startTerm resultTerm  = resultTerm
+
+multiplyTermsHelper firstTerm (MkTerm "Zero" 0 []) resultTerm =  resultTerm
+
+multiplyTermsHelper (MkTerm "succ" 1 [rest]) otherTerm resultTerm= multiplyTermsHelper rest otherTerm (addTerms otherTerm resultTerm)
+ 
+  
+
+
+--PRECONDITION both terms are simplified
+-- append two arrays to each other
+appendArrays :: Term -> Term -> Term
+
+appendArrays (MkTerm "[]" 0 []) otherTerm = otherTerm
+
+appendArrays (MkTerm ":" 2 [x,xs]) otherTerm = (MkTerm ":" 2 [x , appendArrays xs otherTerm])
+
+    
+    
+    
+--Step gaat een vereenvoudigingstap uitvoeren op de term, indien dit niet gaat geeft hij Nothing terug
+-- step1 bevat een fout
+-- step1 voldoet niet aan de eerste property
+step1 :: String-> Term-> Maybe Term
+
+step1 "True" _ = Nothing
+
+step1 "False" _ = Nothing
+
+step1 "Zero" _ = Nothing
+
+step1 "ifThenElse" term  
+    | condition == "True" = Just t2
+    | condition == "False" = Just t3
+    | otherwise =do
+        t1'<- step (name t1) t1
+        return (MkTerm "ifThenElse" 3 (t1' : rest))
+    where
+    (t1:t2:t3:[]) = values term
+    rest =[t2,t3]
+    condition= name t1
+    
+step1 "succ" term = do
+    t' <- step (name t) t
+    return (MkTerm "succ" 1 [t'])
+    where
+    t = head (values term)
+    
+step1 "leq" term 
+    | not (normalForm t1 step2) = do
         t1'<-(step (name t1) t1)
         return (MkTerm "leq" 3 (t1': [t2]))
-    | not (normalForm t2) =do
+    | not (normalForm t2 step2) =do
+        t2'<-(step (name t2) t2)
+        return (MkTerm "leq" 3 (t1:t2': []))
+    | not (nv (name t1) t1) && not (nv (name t1) t1) = Nothing
+    | (name t1 == "Zero") = Just (Variable "X" ) -- Verandering t.o.v. normale step functie
+    | (name t2 == "Zero") = Just (MkTerm "False" 0 [])
+    | (name t1 == "succ") && (name t2 == "succ") = step "leq" (MkTerm "leq" 2 [head (values t1),head (values t2)])
+    | otherwise = Nothing
+     where
+    (t1:t2:[]) = values term
+    
+step1 _ _ = Nothing    
+    
+   
+
+--Step gaat een vereenvoudigingstap uitvoeren op de term, indien dit niet gaat geeft hij Nothing terug
+-- step2 bevat een fout
+-- step2 voldoet niet aan property2
+
+step2 :: String-> Term-> Maybe Term
+
+step2 "True" _ = Nothing
+
+step2 "False" _ = Nothing
+
+step2 "Zero" _ = Nothing
+
+step2 "ifThenElse" term  
+    | condition == "True" = Just t2
+    | condition == "False" = Just t3
+    | otherwise =do
+        t1'<- step (name t1) t1
+        return (MkTerm "ifThenElse" 3 (t1' : rest))
+    where
+    (t1:t2:t3:[]) = values term
+    rest =[t2,t3]
+    condition= name t1
+    
+step2 "succ" term = do
+    t' <- step (name t) t
+    return (MkTerm "succ" 1 [t'])
+    where
+    t = head (values term)
+    
+step2 "leq" term 
+    | not (normalForm t1 step) = do
+        t1'<-(step (name t1) t1)
+        return (MkTerm "leq" 3 (t1': [t2]))
+    | not (normalForm t2 step) =do
+        t2'<-(step (name t2) t2)
+        return (MkTerm "leq" 3 (t1:t2': []))
+    | not (nv (name t1) t1) && not (nv (name t1) t1) = Nothing
+    | (name t1 == "Zero") = Nothing -- Hier zit de fout 
+    | (name t2 == "Zero") = Just (MkTerm "False" 0 [])
+    | (name t1 == "succ") && (name t2 == "succ") = step "leq" (MkTerm "leq" 2 [head (values t1),head (values t2)])
+    | otherwise = Nothing
+     where
+    (t1:t2:[]) = values term
+    
+step2 _ _ = Nothing
+
+
+
+--Step gaat een vereenvoudigingstap uitvoeren op de term, indien dit niet gaat geeft hij Nothing terug
+-- step3 bevat een fout
+-- step3 voldoet niet aan property3
+
+step3 :: String-> Term-> Maybe Term
+
+step3 "True" _ = Nothing
+
+step3 "False" _ = Nothing
+
+step3 "Zero" _ = Nothing
+
+step3 "ifThenElse" term  
+    | condition == "True" = Just t2
+    | condition == "False" = Just t3
+    | otherwise =do
+        t1'<- step (name t1) t1
+        return (MkTerm "ifThenElse" 3 (t1' : rest))
+    where
+    (t1:t2:t3:[]) = values term
+    rest =[t2,t3]
+    condition= name t1
+    
+step3 "succ" term = Just (MkTerm "ifThenElse" 3 [true, true ,true])  -- hier zit de fout 
+    where
+    true = MkTerm "true" 0 []
+    
+step3 "leq" term 
+    | not (normalForm t1 step) = do
+        t1'<-(step (name t1) t1)
+        return (MkTerm "leq" 3 (t1': [t2]))
+    | not (normalForm t2 step) =do
         t2'<-(step (name t2) t2)
         return (MkTerm "leq" 3 (t1:t2': []))
     | not (nv (name t1) t1) && not (nv (name t1) t1) = Nothing
@@ -54,11 +300,13 @@ step "leq" term
      where
     (t1:t2:[]) = values term
     
-step _ _ = Nothing
-    
-normalForm :: Term -> Bool
+step3 _ _ = Nothing
+
    
-normalForm term = (Nothing == (step (name term) term))
+    
+normalForm :: Term -> (String-> Term-> Maybe Term) -> Bool
+   
+normalForm term func = (Nothing == (func (name term) term))
 
 
 
@@ -111,13 +359,9 @@ property2 term func = case t of
     where
     t= func (name term) term
     
---De 3de property zegt dat na de vereenvoudigen moet de term nog steeds aan hastype voldoen    
-{-property3 :: Predicate -> Set [Term] -> Bool
-
-property3 (MkPredicate "hasType" 2 [term,typeOfTerm]) scheme = Set.member [t,typeOfTerm] scheme 
-    where
-    t = eval term-}
     
+    
+--De 3de property zegt dat na de vereenvoudigen moet de term nog steeds aan hastype voldoen    
 
 property3 :: Predicate  -> (String-> Term-> Maybe Term) -> Bool
 
@@ -125,6 +369,8 @@ property3 (MkPredicate "hasType" 2 [term,typeOfTerm]) func = checkHasType t type
     where
     t = eval term func
 
+
+-- Checks if the value of the given type is correct
 checkHasType:: Term -> Term-> Bool
 
 checkHasType (MkTerm "True" 0 [] ) (MkTerm "bool" 0 [] ) =True
